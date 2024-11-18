3f35ea263d15a74e9a9dd3492d2aef786f3d29fa
Argus36 <artemsolomakha2007@gmail.com>
2024-11-18 23:58:15 +0300

    Learned how to output one component many times using an array and learned what props are

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr = [
+  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: 'react-gh-pages/Img/Sneakers/1.jpg' },
+  {name: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: 'react-gh-pages/Img/Sneakers/2.jpg' },
+  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: 'react-gh-pages/Img/Sneakers/3.jpg' },
+  {name: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: 'react-gh-pages/Img/Sneakers/4.jpg' },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -15,9 +22,15 @@ function App() {
             <input placeholder="Поиск"/>
           </div>
         </div>
-        <div className="d-flex justify-between">
+        <div className="d-flex">
 
-          <Card /> {/* Компонент */}
+          {arr.map((obj) => (
+            <Card
+              title = {obj.name}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+            />
+          ))}
 
         </div>
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
-function Card() {
+function Card(props) {
   return (
     <div className="card">
       <div className="favorite">
         <img src="/react-gh-pages/Img/heart-unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src="/react-gh-pages/Img/Sneakers/1.jpg" alt="" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
         <button className="button">
           <img width={11} height={11} src="/react-gh-pages/Img/plus.svg" alt="Plus" />
