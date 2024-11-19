344468b6e13d7b0d30ac4f118ce01edc6238049e
Argus36 <artemsolomakha2007@gmail.com>
2024-11-20 01:23:25 +0300

    Corrected

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
 const arr = [
-  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: 'react-gh-pages/Img/Sneakers/1.jpg' },
-  {name: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: 'react-gh-pages/Img/Sneakers/2.jpg' },
-  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: 'react-gh-pages/Img/Sneakers/3.jpg' },
-  {name: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: 'react-gh-pages/Img/Sneakers/4.jpg' },
+  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: "/react-gh-pages/Img/Sneakers/1.jpg"},
+  {name: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: "/react-gh-pages/Img/Sneakers/2.jpg"},
+  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: "/react-gh-pages/Img/Sneakers/3.jpg"},
+  {name: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: "/react-gh-pages/Img/Sneakers/4.jpg"},
 ];
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <div className="d-flex justify-between align-center mb-40">
           <h1>Все кроссовки</h1>
           <div className="search-block d-flex">
-            <img src="/react-gh-pages/Img/search.svg" alt="Search" />
+            <img src='/react-gh-pages/Img/search.svg' alt="Search" />
             <input placeholder="Поиск"/>
           </div>
         </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -13,7 +13,7 @@ function Card(props) {
       <div className={cardStyle.favorite}>
         <img src="/react-gh-pages/Img/heart-unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src={props.imageUrl} alt="" />
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
       <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
